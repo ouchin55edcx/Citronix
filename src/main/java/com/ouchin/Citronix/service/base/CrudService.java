33c2bb034b410c166284e9b1d30d440b8728b476
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-16 22:15:33 +0100

    Add generic CRUD service interface

---
+++ src/main/java/com/ouchin/Citronix/service/base/CrudService.java
@@ -0,0 +1,12 @@
+package com.ouchin.Citronix.service.base;
+
+import java.util.List;
+
+public interface CrudService<Id, REQUEST_DTO, RESPONSE_DTO> {
+
+    List<RESPONSE_DTO> findAll(int pageNum, int pageSize);
+    RESPONSE_DTO findById(Id id);
+    RESPONSE_DTO create(REQUEST_DTO dto);
+    RESPONSE_DTO update(Id id, REQUEST_DTO dto);
+    void delete(Id id);
+}
