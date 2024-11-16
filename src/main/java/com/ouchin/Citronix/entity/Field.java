443ff6bf75c9b777fa0396941039cfaa45b1da99
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-16 17:05:24 +0100

    CIT-7 create field entity

--- src/main/java/com/ouchin/Citronix/entity/Field.java
+++ src/main/java/com/ouchin/Citronix/entity/Field.java
@@ -1,52 +1,44 @@
 package com.ouchin.Citronix.entity;
 
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name = "farms")
+@Table(name = "fields")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Farm {
+public class Field {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name is required ")
+    @NotNull(message = "Name is required")
+    @NotBlank
     @Column(unique = true, nullable = false)
     private String name;
 
-    @NotBlank(message = "Location is required")
-    @Column(nullable = false)
-    private String location;
-
-    @NotNull(message = "Total area is required ")
-    @Positive
-    @Column(nullable = false)
-    private Double totalArea;
+    @NotNull(message = "Area is required ")
+    @DecimalMin(value = "0.1", message = "Min area of a field it is 0.1 hectare")
+    private Double area;
 
     @NotNull
     @PastOrPresent(message = "The date must be in the past or in the present ")
     @Column(nullable = false)
     private LocalDate creationDate;
 
-
-    @OneToMany(mappedBy = "farm", cascade = CascadeType.ALL)
-    private List<Field> fields = new ArrayList<>();
-
+    @ManyToOne
+    @JoinColumn(name = "farm_id", nullable = false)
+    private Farm farm;
 }
