5d265f6c49217b825ba16d5857308bd7f95d70fb
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-17 12:36:07 +0100

    CIT-20 create a field mapper

---
+++ src/main/java/com/ouchin/Citronix/mapper/FieldMapper.java
@@ -0,0 +1,18 @@
+package com.ouchin.Citronix.mapper;
+
+import com.ouchin.Citronix.dto.request.FieldRequestDTO;
+import com.ouchin.Citronix.dto.respense.FarmResponseDTO;
+import com.ouchin.Citronix.dto.respense.FieldResponseDTO;
+import com.ouchin.Citronix.entity.Farm;
+import com.ouchin.Citronix.entity.Field;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface FieldMapper {
+
+    FieldResponseDTO toResponseDTO(Field field);
+
+    Field toEntity(FieldRequestDTO fieldRequestDTO);
+
+    FarmResponseDTO farmToFarmDto(Farm farm);
+}
