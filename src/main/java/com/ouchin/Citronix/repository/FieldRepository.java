fdad4553607b8a6cd93da5e4acd452fa2a1b407f
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-17 12:37:45 +0100

    CIT-21 create a field repository

---
+++ src/main/java/com/ouchin/Citronix/repository/FieldRepository.java
@@ -0,0 +1,7 @@
+package com.ouchin.Citronix.repository;
+
+import com.ouchin.Citronix.entity.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FieldRepository extends JpaRepository<Field, Long> {
+}
