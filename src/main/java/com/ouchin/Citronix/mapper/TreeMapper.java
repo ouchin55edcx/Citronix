8e9d506f1e519a0cac8596725235e614d87b2dc5
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-18 18:54:17 +0100

    CIT-26 create Tree mapper

--- src/main/java/com/ouchin/Citronix/mapper/TreeMapper.java
+++ src/main/java/com/ouchin/Citronix/mapper/TreeMapper.java
@@ -1,4 +1,19 @@
 package com.ouchin.Citronix.mapper;
 
-public class TreeMapper {
+import com.ouchin.Citronix.dto.request.TreeRequestDTO;
+import com.ouchin.Citronix.dto.respense.TreeResponseDTO;
+import com.ouchin.Citronix.entity.Tree;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TreeMapper {
+
+    TreeResponseDTO toResponseDTO(Tree tree);
+
+    List<TreeResponseDTO> toResponseDTOs(List<Tree> trees);
+
+    Tree toEntity(TreeRequestDTO treeRequestDTO);
+
 }
