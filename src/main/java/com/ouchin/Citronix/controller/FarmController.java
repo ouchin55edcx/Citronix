b7ff9d4bb47c35741417dea72d4260acfa7853bb
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-18 16:04:36 +0100

    Update in farm controller

--- src/main/java/com/ouchin/Citronix/controller/FarmController.java
+++ src/main/java/com/ouchin/Citronix/controller/FarmController.java
@@ -3,12 +3,6 @@ package com.ouchin.Citronix.controller;
 import com.ouchin.Citronix.dto.request.FarmRequestDTO;
 import com.ouchin.Citronix.dto.respense.FarmResponseDTO;
 import com.ouchin.Citronix.service.FarmService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,15 +17,7 @@ public class FarmController {
     @Autowired
     private FarmService farmService;
 
-    @Operation(
-            summary = "Retrieve all farms",
-            description = "Get a list of all farms in the system."
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Successful retrieval of farms",
-                    content = @Content(schema = @Schema(implementation = FarmResponseDTO.class))),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
+
     @GetMapping
     public ResponseEntity<List<FarmResponseDTO>> getAllFarms() {
         List<FarmResponseDTO> farms = farmService.findAll();
@@ -39,16 +25,6 @@ public class FarmController {
     }
 
 
-    @Operation(
-            summary = "Get a farm by ID",
-            description = "Retrieve a specific farm using its ID."
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Successful retrieval of farm",
-                    content = @Content(schema = @Schema(implementation = FarmResponseDTO.class))),
-            @ApiResponse(responseCode = "404", description = "Farm not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
     @GetMapping("/{id}")
     public ResponseEntity<FarmResponseDTO> getFarmById(@PathVariable Long id) {
         FarmResponseDTO farm = farmService.findById(id);
@@ -59,16 +35,7 @@ public class FarmController {
     }
 
 
-    @Operation(
-            summary = "Create a new farm",
-            description = "Create a new farm with the provided details."
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "Farm created successfully",
-                    content = @Content(schema = @Schema(implementation = FarmResponseDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Invalid input"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
+
     @PostMapping
     public ResponseEntity<FarmResponseDTO> createFarm(@RequestBody FarmRequestDTO farmRequestDTO) {
         FarmResponseDTO createdFarm = farmService.create(farmRequestDTO);
@@ -76,17 +43,7 @@ public class FarmController {
     }
 
 
-    @Operation(
-            summary = "Update a farm",
-            description = "Update an existing farm's details by its ID."
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Farm updated successfully",
-                    content = @Content(schema = @Schema(implementation = FarmResponseDTO.class))),
-            @ApiResponse(responseCode = "404", description = "Farm not found"),
-            @ApiResponse(responseCode = "400", description = "Invalid input"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
+
     @PutMapping("/{id}")
     public ResponseEntity<FarmResponseDTO> updateFarm(
             @PathVariable Long id,
@@ -96,15 +53,7 @@ public class FarmController {
     }
 
 
-    @Operation(
-            summary = "Delete a farm",
-            description = "Delete a farm by its ID."
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "Farm deleted successfully"),
-            @ApiResponse(responseCode = "404", description = "Farm not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFarm(@PathVariable Long id) {
         farmService.delete(id);
@@ -113,17 +62,7 @@ public class FarmController {
 
 
 
-    @Operation(
-            summary = "Search a farms",
-            description = "Search a farm by name or location "
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Search results found",
-                    content = @Content(schema = @Schema(implementation = FarmResponseDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
 
-    })
     @GetMapping("/search")
     public ResponseEntity<List<FarmResponseDTO>> searchFarms(
             @RequestParam(required = false) String name,
