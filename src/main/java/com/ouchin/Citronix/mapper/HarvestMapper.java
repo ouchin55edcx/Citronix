630bafdef26ddade8de2ba514b7f371386554bfc
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-19 16:57:04 +0100

    CIT-32 create harvest mapper

--- src/main/java/com/ouchin/Citronix/mapper/HarvestMapper.java
+++ src/main/java/com/ouchin/Citronix/mapper/HarvestMapper.java
@@ -1,4 +1,35 @@
 package com.ouchin.Citronix.mapper;
 
-public class HarvestMapper {
+import com.ouchin.Citronix.dto.request.HarvestDetailsRequestDTO;
+import com.ouchin.Citronix.dto.request.HarvestRequestDTO;
+import com.ouchin.Citronix.dto.respense.HarvestDetailsResponseDTO;
+import com.ouchin.Citronix.dto.respense.HarvestResponseDTO;
+import com.ouchin.Citronix.entity.Harvest;
+import com.ouchin.Citronix.entity.HarvestDetails;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface HarvestMapper {
+
+    @Mapping(target = "harvestDetails", source = "harvestDetails")
+    HarvestResponseDTO toResponseDTO(Harvest harvest);
+
+    Harvest toEntity(HarvestRequestDTO requestDTO);
+
+    @Mapping(target = "harvestId", source = "details.id.harvestId")
+    @Mapping(target = "treeId", source = "details.id.treeId")
+    HarvestDetailsResponseDTO toResponseDTO(HarvestDetails details);
+
+    HarvestDetails toEntity(HarvestDetailsRequestDTO requestDTO);
+
+    List<HarvestResponseDTO> toHarvestResponseDTOList(List<Harvest> harvests);
+
+    List<HarvestDetailsResponseDTO> toHarvestDetailsResponseDTOList(List<HarvestDetails> details);
 }
