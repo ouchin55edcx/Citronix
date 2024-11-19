4ac64939f90008aee7ba8850f365182d64f6f874
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-19 17:03:02 +0100

    CIT-33 create harvest & harvest details repositories

--- src/main/java/com/ouchin/Citronix/repository/HarvestDetailsRepository.java
+++ src/main/java/com/ouchin/Citronix/repository/HarvestDetailsRepository.java
@@ -1,4 +1,12 @@
 package com.ouchin.Citronix.repository;
 
-public class HarvestDetailsRepository {
+import com.ouchin.Citronix.entity.HarvestDetails;
+import com.ouchin.Citronix.entity.Tree;
+import com.ouchin.Citronix.entity.embedded.HarvestDetailsId;
+import com.ouchin.Citronix.entity.enums.Season;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HarvestDetailsRepository extends JpaRepository<HarvestDetails, HarvestDetailsId> {
+    boolean existsByTreeAndHarvestSeason(Tree tree, Season season);
+
 }
--- src/main/java/com/ouchin/Citronix/repository/HarvestRepository.java
+++ src/main/java/com/ouchin/Citronix/repository/HarvestRepository.java
@@ -1,4 +1,13 @@
 package com.ouchin.Citronix.repository;
 
-public interface HarvestRepository {
+import com.ouchin.Citronix.entity.Harvest;
+import com.ouchin.Citronix.entity.enums.Season;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HarvestRepository extends JpaRepository<Harvest, Long> {
+    List<Harvest> findBySeason(Season season);
+
 }
+
