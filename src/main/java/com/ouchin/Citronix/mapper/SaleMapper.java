8c0dfb3174dc9cc1b93e95ff5e1fc8c0db637b9b
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-20 11:54:31 +0100

    CIT-38 Create sale mapper

--- src/main/java/com/ouchin/Citronix/mapper/SaleMapper.java
+++ src/main/java/com/ouchin/Citronix/mapper/SaleMapper.java
@@ -1,4 +1,27 @@
 package com.ouchin.Citronix.mapper;
 
-public class SaleMapper {
+import com.ouchin.Citronix.dto.request.SaleRequestDTO;
+import com.ouchin.Citronix.dto.respense.SaleResponseDTO;
+import com.ouchin.Citronix.entity.Sale;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring")
+public interface SaleMapper {
+    @Mapping(target = "harvest", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Sale toEntity(SaleRequestDTO dto);
+
+    @Mapping(target = "revenue", expression = "java(calculateRevenue(sale))")
+    @Mapping(target = "harvestId", source = "harvest.id")
+    @Mapping(target = "harvestDate", source = "harvest.harvestDate")
+    @Mapping(target = "season", source = "harvest.season")
+    SaleResponseDTO toDTO(Sale sale);
+
+    @Named("calculateRevenue")
+    default Double calculateRevenue(Sale sale) {
+        return sale.getQuantity() * sale.getPrixUnitaire();
+    }
 }
+
