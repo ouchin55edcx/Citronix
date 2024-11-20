b00ea7df590198254da04a282486edea09450605
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-20 11:56:04 +0100

    CIT-39 Create Sale repositorey

--- src/main/java/com/ouchin/Citronix/repository/SaleRepository.java
+++ src/main/java/com/ouchin/Citronix/repository/SaleRepository.java
@@ -1,4 +1,9 @@
 package com.ouchin.Citronix.repository;
 
-public interface SaleRepository {
+import com.ouchin.Citronix.entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 }
